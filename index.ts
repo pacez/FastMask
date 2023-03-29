8188c27674075bc1d97916483452bd60fcf9f8e0
Pace Zhong <pace_zhong@foxmail.com>
2023-03-29 14:47:13 +0800

    Create index.ts
    init

---
+++ index.ts
@@ -0,0 +1,37 @@
+/**
+ *  FastMask Code snippets
+ *  Author: pace_zhong@foxmail.com
+ */
+
+export default class FastMask {
+    constructor() {
+        this.init();
+    }
+
+    styles:Partial<CSSStyleDeclaration> = {
+        position: 'fixed',
+        zIndex: '999',
+        width: '100%',
+        height: '100%',
+        top: '0', 
+        left: '0',
+        background: 'rgba(255,255,255,0)',
+    }
+
+    $mask = document.createElement('div');
+
+    init = () => {
+        const { styles } = this;
+        for (let name in styles) {
+            this.$mask.style[name] = styles[name]!;
+        }
+    }
+
+    show = () => {
+        document.body.appendChild(this.$mask);
+    }
+
+    destroy = () => {
+        document.body.removeChild(this.$mask);
+    }
+}
